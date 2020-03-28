f10752e32c1c6f87232f59c54637f1cd8c850335
Albert Sánchez <a.sanpra@gmail.com>
2020-03-28 14:15:48 +0100

    day 2 - about - portfolio / single - contact

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
         //Slide right/left menu
         $('header nav').toggleClass('hide');
     });
+    
+    $('.portfolio .filter span').click(function(){
+        //console.log('Clicked', this);
+        var classe = $(this).attr('class');
+        console.log(classe);
+        
+        if (classe=='all'){
+            $('.portfolio .item').show();
+            $('.portfolio .filter span').removeClass('active');
+            $(this).addClass('active');
+        }else{
+            if ($(this).hasClass('active')){
+            }else{
+                $('.portfolio .items .item').hide();
+                $('.portfolio .filter span').removeClass('active');
+                $(this).addClass('active');
+                $('.portfolio .'+classe).show();
+            }
+        }
+    });
 });
